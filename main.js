a45def34d1c27ea38605dace58cafbbdcc00cb71
julien-camara <juliencamara1999@gmail.com>
2022-12-09 07:37:33 +0100

    modification du fichier main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 alert('je vous salue tous !')
+
+const formalaire=document.getElementById('maForm')
+
+formalaire.addEventListener('submit',(e)=>{
+    const data=e.target.value
+    var requete={
+        url:`http://localhost:3000`,
+        method:"post",
+        data:data
+    }
+    if(confirm('Voulez-vous vraiment envoyer les données de ce formulaire ?')){
+        $.ajax(requete).done()
+    }
+})
